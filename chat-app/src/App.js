a399d67de1621707d9be7eb1047e7807e5294a95
Husny Ahamed <gazzali@omobio.net>
2018-09-30 21:07:38 +0530

    Designed a simple ui. Created soket io client module with functions to emit and receive messages through server. Handled simple chat functionality. Styling and chat list WIP

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,18 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketClient from "./socket-client/socket-client";
+const socket = socketClient();
 
 class App extends Component {
+  state = {
+    chatInput: ""
+  };
+
+  componentDidMount() {
+    this.nameInput.focus();
+    socket.registerHandler(message => {
+      console.log(message);
+    });
+  }
+
+  handleKeyPress = event => {
+    if (event.key == "Enter") {
+      event.preventDefault();
+
+      socket.message("TestChat", this.state.chatInput, function() {
+        console.log("Message sent");
+      });
+
+      this.setState({
+        chatInput: ""
+      });
+    }
+  };
+
+  onTextInput = event => {
+    this.setState({
+      chatInput: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React World, by Husny</h1>
+          <h1 className="App-title">Fast Chat by Husny Ahamed</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="chatmsgwrapper">
+          <textarea
+            ref={input => {
+              this.nameInput = input;
+            }}
+            onKeyPress={this.handleKeyPress}
+            className="chatmsg disabled"
+            onChange={this.onTextInput}
+            value={this.state.chatInput}
+            cols="80"
+            rows="1"
+            disabled=""
+          />
+        </div>
       </div>
     );
   }

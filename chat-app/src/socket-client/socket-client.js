e8a86d5a2351e8d314a3d5ced720c3599c49c274
Husny Ahamed <gazzali@omobio.net>
2018-10-01 00:48:01 +0530

    Added server endpoint of hosted heroku server.

--- chat-app/src/socket-client/socket-client.js
+++ chat-app/src/socket-client/socket-client.js
@@ -1,7 +1,8 @@
 const io = require("socket.io-client");
 
 export default function() {
-  const socket = io.connect("http://localhost:8500");
+  // const socket = io.connect("http://localhost:8500");
+  const socket = io.connect("https://murmuring-cove-55442.herokuapp.com/");
 
   function subscribeToMessages(onMessageReceived) {
     socket.on("message", onMessageReceived);

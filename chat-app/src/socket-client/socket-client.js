49178545e71078723a716f0cb250870f500ff9dd
Husny Ahamed <gazzali@omobio.net>
2018-10-01 20:35:24 +0530

    Pointed live server

--- chat-app/src/socket-client/socket-client.js
+++ chat-app/src/socket-client/socket-client.js
@@ -1,8 +1,8 @@
 const io = require("socket.io-client");
 
 export default () => {
-  const socket = io.connect("http://localhost:8500");
-  // const socket = io.connect("https://murmuring-cove-55442.herokuapp.com/");
+  // const socket = io.connect("http://localhost:8500");
+  const socket = io.connect("https://murmuring-cove-55442.herokuapp.com/");
 
   function subscribeToMessages(onMessageReceived) {
     socket.on("message", onMessageReceived);
